da6fe83d8461bcc91c5fe422c5d83bc48ef1446e
GyeongJun <kilo1418@gmail.com>
2015-05-04 02:32:39 -0400

    revert handler_manage.js

--- handler_manage.js
+++ handler_manage.js
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-$(function(){
-
-	$(document).ready(init);
-	$(window).on('hashchange', function() {
-		hashChange();
-		// on 메소드의 두번째 인자로 함수명 전달시 
-		// onload와 동일하게 동작하므로 일단 이렇게 해둠.
-	});
-
-	$(window).on('load', function() {
-		$(window).scrollTop(0);
-		hashChange("onLoad");
-	});
-	
-
-
-	
-})
-
-function init() {
-
-//  Initialize management menu
-	$('#notice').load("notice_manage.html");
-	$('#prob').load("prob_manage.html");
-	$('#user').load("user_manage.html");
-	$('#hall').load("hall_manage.html");
-
-}
-
-function hashChange(caller) {
-	
-	var tag = window.location.hash.substr(1, window.location.hash.length);
-	if( tag=="" && caller=="onLoad") {
-		//window.location.hash = "Notice"
-		//window.location.reload();
-		return ;
-	}
-
-	//$('#main').empty();
-	//$('#main').load(tag + ".html");	//web.xml을 통해 다른 이름으로 매핑하자.
-
-
-}
-
-
-function loadNotice(box) {
-	
-=======
 $(function(){
 
 	$(document).ready(init);
@@ -116,5 +67,4 @@ function setProblemList(data) {
 		console.log(content);		
 	}
 
->>>>>>> origin/master
 }
